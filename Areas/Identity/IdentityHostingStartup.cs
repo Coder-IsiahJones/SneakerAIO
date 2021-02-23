af56fd97a13c89582b2c5de806800b43c6eb9a63
Coder-IsiahJones <jones.isiah.blhs@gmail.com>
2021-02-23 14:01:33 -0500

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SneakerAIO.Areas.Identity.Data;
+using SneakerAIO.Data;
+
+[assembly: HostingStartup(typeof(SneakerAIO.Areas.Identity.IdentityHostingStartup))]
+namespace SneakerAIO.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SneakerDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SneakerDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+
+                    .AddEntityFrameworkStores<SneakerDbContext>();
+            });
+        }
+    }
+}
---
+++ Migrations/20210223184017_SneakerDb table change.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SneakerAIO.Migrations
+{
+    public partial class SneakerDbtablechange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "strFirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "strUserName",
+                table: "AspNetUsers",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "strLastName",
+                table: "AspNetUsers",
+                newName: "FirstName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "AspNetUsers",
+                newName: "strUserName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "AspNetUsers",
+                newName: "strLastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "strFirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                nullable: true);
+        }
+    }
+}
